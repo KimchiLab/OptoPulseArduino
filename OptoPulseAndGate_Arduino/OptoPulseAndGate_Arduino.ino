635acee2a97299bdf24cd4e8df2bcd46e13f74cc
Eyal Kimchi Lab <34275765+KimchiLab@users.noreply.github.com>
2019-08-13 12:16:27 -0400

    Exit pulse loop if gate goes low during loop

--- OptoPulseAndGate_Arduino/OptoPulseAndGate_Arduino.ino
+++ OptoPulseAndGate_Arduino/OptoPulseAndGate_Arduino.ino
@@ -15,8 +15,7 @@ const int optoPin = LED_BUILTIN; // Optogenetics laser trigger: LED_BUILTIN = 13
 const int pulsePin = 2; // Input TTL: when on, opto triggered if gatePin is also on. Mega: Interrupt Pins = 2, 3, 18, 19, 20, 21. https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
 const int gatePin = 3; // Input TTL: when on, opto triggered for each pulse on pulsePin. Mega: Interrupt Pins = 2, 3, 18, 19, 20, 21. https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
 
-volatile bool gateState, pulseState, pulseLast, optoOn;
-unsigned long us_on;
+unsigned long us_delay = 2 * 1000; // Short delay so as not to overlap with ongoing imaging, shouldn't be necessary
 unsigned long us_dur = 5 * 1000;
 
 // BEGINNNING OF FUNCTIONS
@@ -25,60 +24,26 @@ void setup() {
   pinMode(optoPin, OUTPUT);
   pinMode(pulsePin, INPUT_PULLUP);
   pinMode(gatePin, INPUT_PULLUP);
-
-  noInterrupts();
-  GateState(); // Initialize gate state, afterwards will only check with changes
-  PulseState(); // Initialize gate state, afterwards will only check with changes
-  attachInterrupt(digitalPinToInterrupt(gatePin), GateState, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(pulsePin), PulseState, CHANGE);
-  interrupts(); // Restart interrupts: ready to go!
-
-  Serial.begin(115200);
 }
 
-void loop() { // If gate active, pulse was not on, but now is on, then give a pulse!
-//  unsigned long us = micros();
-  if (gateState && !pulseLast && pulseState && !optoOn) {
-    digitalWriteFast(optoPin, HIGH);
-    us_on = micros();
-    optoOn = true;
-    pulseLast = true;
-//    Serial.print("Pulse On ");
-//    Serial.print(" ");
-//    Serial.print(us);
-//    Serial.print(" ");
-//    Serial.print(us_on);
-//    Serial.print(" ");
-//    Serial.print(us_on - us);
-//    Serial.println();
+void loop() {
+  while (digitalReadFast(gatePin) == HIGH) { // Gate active: Enter pulse montoring loop
+    while (digitalReadFast(pulsePin)  == HIGH) { } // Wait until pulse inactive to restart pulse monitoring loop. Assuming can not go low and then high again during pulse delay & duration
+    if (digitalReadFast(gatePin) == LOW) {
+      // Make sure gatePin is not low. If it is low, then exit loop early, otherwise will cont to wait for next pulse, even after done
+      break;
+    }
+    while (digitalReadFast(pulsePin)  == LOW) { }// Wait for a clean entry into the first pulse: otherwise may start at the end of a frame: Inherent delay unfortunatley, but necessary for clean imaging to wait for transition from low to high
+    if (digitalReadFast(gatePin) == LOW) {
+      // Make sure gatePin is not low. If it is low, then exit loop early, don't proceed to pulse
+      break;
+    }
+    // Once a pulse is initiated, complete it
+    unsigned long us_trigger = micros(); // Mark onset of delay
+    while (micros() - us_trigger < us_delay) { } // Loop until delay has passed
+    digitalWriteFast(optoPin, HIGH); // Start pulse
+    unsigned long us_pulse = micros(); // Mark onset of pulse
+    while (micros() - us_pulse < us_dur) { } // Loop until pulse duration has passed
+    digitalWriteFast(optoPin, LOW); // End pulse
   }
-  
-  if (optoOn && (micros() - us_on >= us_dur)) {
-    digitalWriteFast(optoPin, LOW);
-    optoOn = false;
-//    Serial.print("Pulse Off ");
-//    Serial.print(micros());
-//    Serial.print(" ");
-//    Serial.print(us_on);
-//    Serial.print(" ");
-//    Serial.print(us_dur);
-//    Serial.print(" ");
-//    Serial.print(micros() - us_on);
-//    Serial.println();
-  }
-
-  if (pulseLast && !pulseState && !optoOn) {
-    pulseLast = false;
-  }
-}
-
-
-// Register/save state of gate depending on new state, minimize repeated reading of gatePin
-void GateState() {
-  gateState = digitalReadFast(gatePin);
-}
-
-// Register/save state of gate depending on new state, minimize repeated reading of gatePin
-void PulseState() {
-  pulseState = digitalReadFast(pulsePin);
 }
